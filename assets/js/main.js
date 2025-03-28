abda8f341fda7f4bd5e471a9420fa02a1b5bcac7
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-03-28 17:55:21 +0100

    la creation nav et header

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+/*=============== SHOW MENU ===============*/
+ const navMenue = document.getElementById('nav-menu')
+       navClose = document.getElementById('nav-close')
+       navtoggle = document.getElementById('nav-toggle')
+
+    if(navtoggle){
+        navtoggle.addEventListener('click' ,()=>{
+            navMenue.classList.add('show-menu')
+            console.log(navMenue);
+            
+        })
+
+    }
+    if(navClose){
+        navClose.addEventListener('click', ()=>{
+            navMenue.classList.remove('show-menu')
+            console.log(navClose);
+            
+        })
+    }
+ 
+ /*=============== REMOVE MENU MOBILE ===============*/
+ 
+ 
+ /*=============== ADD SHADOW HEADER ===============*/
+ 
+ 
+ /*=============== SWIPER POPULAR ===============*/
+ 
+ 
+ /*=============== SHOW SCROLL UP ===============*/ 
+ 
+ 
+ /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+ 
+ 
+ /*=============== SCROLL REVEAL ANIMATION ===============*/
