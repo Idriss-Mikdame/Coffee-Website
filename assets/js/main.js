e38da75876dcc0af12004a988e766453006d5d09
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-04-03 16:26:51 +0200

    sroll section

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,30 @@
     });
  
  /*=============== SHOW SCROLL UP ===============*/ 
- 
- 
+ const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+ }
+ window.addEventListener('scroll',scrollUp)
  /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+ const section =  document.querySelectorAll('section[id]')
  
- 
+ const scrollActive = ()=>{
+    const scrollDown = window.scrollY
+
+    section.forEach(current =>{
+        const sectionbHeights = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+              
+              if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionbHeights){
+                sectionClass.classList.add('active-link')
+              }else{
+                sectionClass.classList.remove('active-link')
+              }
+    } )
+ }
+ window.addEventListener('scroll',scrollActive)
  /*=============== SCROLL REVEAL ANIMATION ===============*/
